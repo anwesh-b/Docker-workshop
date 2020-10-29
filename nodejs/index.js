6a18e70d7f36a36c85de38948f824fb9ae289b85
Anwesh Budhathoki <abperni@gmail.com>
2020-10-29 10:57:39 +0545

    node:v1

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/',(req,res)=>{
-    // var date = new Date();
-    res.send("Server running");
-    //console.log("Requet at ",date.getFullYear(),date.getMonth(),date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds())
+    res.send("Welcome to Docker Express");
 })
 
 app.listen(port,()=> console.log(`Server running at port ${port}`));
